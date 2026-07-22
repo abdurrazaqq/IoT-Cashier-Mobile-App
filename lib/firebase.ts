dfdb7f3dd8218f454adbe0ef069823d3327498a7
abdurrazaqq <razaqjr10@gmail.com>
2026-07-23 02:11:49 +0700

    Move Firebase configuration to environment variables

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -3,13 +3,12 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCuQDgPrLcTE9NhKjzMXhEnPhnq0fwYaaU",
-  authDomain: "database-kasir-a46d8.firebaseapp.com",
-  projectId: "database-kasir-a46d8",
-  storageBucket: "database-kasir-a46d8.firebasestorage.app",
-  messagingSenderId: "694407241213",
-  appId: "1:694407241213:web:8c82f434808c6095570d94",
-  measurementId: "G-FG1HGSLB23"
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
